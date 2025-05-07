3745b9dfeccc1e68da35528a8b5b4838b6a7dd57
Dinma Faith <phaytep@gmail.com>
2025-05-07 22:58:07 +0100

    Remove unused files and delete comments

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import "./css/styles.css"
-// import './index.css'
-// import App from '../vite-project/src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
